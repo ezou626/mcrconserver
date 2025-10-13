88df544bafbe742a88340283eff89637bf4750ad
Eric Zou <ezou626@gmail.com>
2025-10-13 15:07:34 -0400

    simple working fastapi server

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+import logging
+import uvicorn
 
-app = FastAPI()
+app = FastAPI(title="MCRConServer API", version="0.0.1")
+
+LOG = logging.getLogger(__name__)
+LOG.info("API is starting up")
+LOG.info(uvicorn.Config.asgi_version)
 
 
 @app.get("/")
