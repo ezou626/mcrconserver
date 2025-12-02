afeaced23af8d528afc19857b6836a60455d4731
Eric Zou <ezou626@gmail.com>
2025-12-02 08:25:35 -0500

    remove ability to send null commands, integrate QueueCommandResult

--- backend/app/rconclient/client.py
+++ backend/app/rconclient/client.py
@@ -5,31 +5,29 @@ Since all modules are singletons, we get a natural shared client across the app.
 """
 
 import asyncio
+from asyncio.queues import QueueShutDown
 import logging
 
 from .worker import queue
 from .command import RCONCommand
+from .types import QueueCommandResult
 from ..auth.user import User
 
 LOG = logging.getLogger(__name__)
 LOG.setLevel(logging.DEBUG)
 
 
-async def queue_command(
-    command: str | None, user: User, require_result: bool = True
-) -> dict:
+async def queue_command(command: str, user: User) -> QueueCommandResult:
     """
     Queues a command to be sent to the RCON server.
 
     Args:
         command: The command string to send.
+        user: The User sending the command.
 
     Returns:
-        A dictionary with the result of the command execution, good for HTTP response.
+        A QueueCommandResult object for that command.
     """
-
-    if not command:
-        return {"processed": False, "message": "No command provided"}
 
     LOG.debug("Queueing command: %s by %s", command, user.username)
     future = asyncio.get_running_loop().create_future()
@@ -37,10 +35,22 @@ async def queue_command(
 
     try:
         queue.put_nowait(task)
-        if not require_result:
-            return {"queued": True, "message": "Command queued successfully"}
-        result = await future
-        return {"processed": True, "message": result}
+        return QueueCommandResult(
+            command=task,
+            queued=True,
+            message="Command queued successfully",
+        )
+    except QueueShutDown:
+        LOG.error("Queue terminated unexpectedly")
+        return QueueCommandResult(
+            command=task,
+            queued=False,
+            message="Queue terminated unexpectedly",
+        )
     except Exception as e:
         LOG.error("Failed to queue command: %s", e)
-        return {"processed": False, "message": str(e)}
+        return QueueCommandResult(
+            command=task,
+            queued=False,
+            message=str(e),
+        )
