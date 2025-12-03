347db9630bcc637cffd55c8b5d74256d03960b02
Eric Zou <ezou626@gmail.com>
2025-12-02 22:41:32 -0500

    reduce file scatter

--- backend/app/auth/user.py
+++ backend/app/auth/user.py
@@ -1,10 +1,23 @@
-"""Fundamental user data structure."""
+"""Fundamental data model for auth"""
 
 from dataclasses import dataclass
-from .roles import Role
+from enum import IntEnum
+
+
+class Role(IntEnum):
+    """User roles with hierarchical permissions."""
+
+    OWNER = 0
+    ADMIN = 1
+    USER = 2
+
+    def check_permission(self, required_role: "Role") -> bool:
+        return self.value <= required_role.value
 
 
 @dataclass
 class User:
+    """Data structure representing a user."""
+
     username: str
     role: Role
