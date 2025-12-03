7e59e5ba5e75bae5034013839de81ab9dfa1fa67
Eric Zou <ezou626@gmail.com>
2025-12-02 22:54:20 -0500

    add flexible db path

--- backend/app/auth/db_connection.py
+++ backend/app/auth/db_connection.py
@@ -4,25 +4,25 @@ from sqlite3 import connect
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.DEBUG)
 
-db_path = None
-db = None
+_db_path = None
+_db = None
 
 
 def set_db_path(path: str) -> None:
-    global db_path, db
-    db_path = path
-    LOGGER.debug(f"Database path set to: {db_path}")
-    if db is not None:
-        db.close()
-        db = None
+    global _db_path, _db
+    _db_path = path
+    LOGGER.debug(f"Database path set to: {_db_path}")
+    if _db is not None:
+        _db.close()
+        _db = None
 
 
 def get_db_connection():
-    global db
-    if db is not None:
-        return db
-    if db_path is None:
+    global _db
+    if _db is not None:
+        return _db
+    if _db_path is None:
         raise RuntimeError("Database path is not set.")
-    db = connect(db_path, check_same_thread=False)
-    LOGGER.debug(f"Database connection established to: {db_path}")
-    return db
+    _db = connect(_db_path, check_same_thread=False)
+    LOGGER.debug(f"Database connection established to: {_db_path}")
+    return _db
