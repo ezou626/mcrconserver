945403e89ec36a9099ad52aa2d4a434729916c32
Eric Zou <ezou626@gmail.com>
2025-12-05 13:52:27 -0500

    actually i need incorrect password exception

--- backend/app/rconclient/rcon_exceptions.py
+++ backend/app/rconclient/rcon_exceptions.py
@@ -5,3 +5,9 @@ class RCONClientMissingPassword(Exception):
     """Raised when the RCON password is missing from environment variables."""
 
     pass
+
+
+class RCONClientIncorrectPassword(Exception):
+    """Raised when the RCON password is incorrect"""
+
+    pass
