d448f943cc4a29e38901d1768e12183794b65b65
Eric Zou <ezou626@gmail.com>
2025-12-06 23:07:15 -0500

    formatting

--- backend/app/src/rconclient/__init__.py
+++ backend/app/src/rconclient/__init__.py
@@ -1,12 +1,10 @@
-"""
-Provides async RCON execution functionality for backend operations.
-"""
+"""Provides async RCON execution functionality for backend operations."""
 
 from .types import RCONCommand
 from .worker import RCONWorkerPool, RCONWorkerPoolConfig
 
 __all__ = [
+    "RCONCommand",
     "RCONWorkerPool",
     "RCONWorkerPoolConfig",
-    "RCONCommand",
 ]
